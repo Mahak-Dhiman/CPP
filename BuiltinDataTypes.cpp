00ff23ed37e8952945e5ac54231e20ae4db884ac
Mahak Dhiman <58025022+Mahak-Dhiman@users.noreply.github.com>
2021-02-17 22:15:01 +0530

    Add files via upload

---
+++ BuiltinDataTypes.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a = 4;
+	int b = 5;
+	cout<<"The value of a is "<<a<<" and b is "<<b;
+	float pi = 3.14;
+	cout<<"\n The value of pi is "<<pi;
+	char c = 'u';
+	cout<<"\n The value of c is "<<c;
+	bool bo = false;
+	cout<<"\n The value of bo is "<<bo;
+	double d = 2.34287922;
+	cout<<"\n The value of d is "<<d;
+	return 0;
+}
